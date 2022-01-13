cdbc95d74f9243a74c70ddbca4841fe90d289e45
sanwar786 <95489644+sanwar786@users.noreply.github.com>
2022-01-13 19:03:35 +0530

    Swaping Two Number

---
+++ FreshTest102Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshTest102Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapTwoNumbers.SwapingNumbers();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FreshTest102Project/SwapTwoNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshTest102Project
+{
+    //Each Word 1st Letter has to Start With UpperCase 
+    //Pascal Case
+    //Pascal Will Follow For Classname And Method Name 
+    internal class SwapTwoNumbers
+    {
+        public static void SwapingNumbers()
+        {
+            //Camal Case For Local Variable And Method Parameter
+            int firstNum, secondNum, temp = 0;
+            Console.WriteLine("Please Enter First Number");
+            firstNum = Convert.ToInt32(Console.ReadLine()); //10
+            Console.WriteLine("Please Enter Second Number");
+            secondNum = Convert.ToInt32(Console.ReadLine()); //20
+
+            Console.WriteLine("Before Exchanging Values: first{0}, second={1}", firstNum, secondNum);
+            temp = firstNum; //10
+            firstNum = secondNum; //20
+            secondNum = temp; //10
+            Console.WriteLine("After Exchanging Values: first={0}, second={1}", firstNum, secondNum);
+        }
+    }
+}
